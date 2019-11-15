4eace3f5195e61e26ed3220958134d2dc0c38a9e
子春 <2391962829@qq.com>
2019-11-15 14:08:07 +0800

    第二次提交

---
+++ src/pages/Find.js
@@ -0,0 +1,11 @@
+import React from "react"
+
+class Find extends React.Component {
+    render() {
+        return (
+            <h1>发现</h1>
+        )
+    }
+}
+
+export default Find
---
+++ src/pages/My.js
@@ -0,0 +1,11 @@
+import React from "react"
+
+class My extends React.Component{
+    render(){
+        return (
+            <h1>我的</h1>
+        )
+    }
+}
+
+export default My
